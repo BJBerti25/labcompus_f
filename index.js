a49beb15dde6a9001b5db32c0cfcb913211bde3d
BJBerti25 <justibertini@gmail.com>
2025-06-08 22:18:11 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,172 @@
+function precio1(){
+   let preciobase = 4900;
+   let cantidad = parseInt(document.getElementById("cantidad").value);
+   let preciototal = preciobase * cantidad;
+   document.getElementById("preciototal").value = preciototal;
+}
+
+function precio2(){
+   let preciobase = 4900;
+   let cantidad = parseInt(document.getElementById("cantidad2").value);
+   let preciototal2 = preciobase * cantidad;
+   document.getElementById("preciototal2").value = preciototal2;
+}
+
+function precio3(){
+   let preciobase = 4900;
+   let cantidad = parseInt(document.getElementById("cantidad3").value);
+   let preciototal3 = preciobase * cantidad;
+   document.getElementById("preciototal3").value = preciototal3;
+}
+
+function precio4(){
+   let preciobase2 = 24000;
+   let cantidad = parseInt(document.getElementById("cantidad4").value);
+   let kilo = parseInt(document.getElementById("kilo4").value);
+   let preciototal4 = (preciobase2 * kilo) * cantidad;
+   document.getElementById("preciototal4").value = preciototal4;
+}
+
+function precio5(){
+   let preciobase2 = 24000;
+   let cantidad = parseInt(document.getElementById("cantidad5").value);
+   let kilo = parseInt(document.getElementById("kilo5").value);
+   let preciototal5 = (preciobase2 * kilo) * cantidad;
+   document.getElementById("preciototal5").value = preciototal5;
+}
+
+function precio6(){
+   let preciobase2 = 24000;
+   let cantidad = parseInt(document.getElementById("cantidad6").value);
+   let kilo = parseInt(document.getElementById("kilo6").value);
+   let preciototal6 = (preciobase2 * kilo) * cantidad;
+   document.getElementById("preciototal6").value = preciototal6;
+}
+
+function precio7(){
+   let preciobase2 = 24000;
+   let cantidad = parseInt(document.getElementById("cantidad7").value);
+   let kilo = parseInt(document.getElementById("kilo7").value);
+   let preciototal7 = (preciobase2 * kilo) * cantidad;
+   document.getElementById("preciototal7").value = preciototal7;
+}
+
+function precio8(){
+   let preciobase2 = 24000;
+   let cantidad = parseInt(document.getElementById("cantidad8").value);
+   let kilo = parseInt(document.getElementById("kilo8").value);
+   let preciototal8 = (preciobase2 * kilo) * cantidad;
+   document.getElementById("preciototal8").value = preciototal8;
+}
+
+function precio9(){
+   let preciobase = 30000;
+   let cantidad = parseInt(document.getElementById("cantidad9").value);
+   let preciototal9 = preciobase * cantidad;
+   document.getElementById("preciototal9").value = preciototal9;
+}
+
+function precio10(){
+   let preciobase = 30000;
+   let cantidad = parseInt(document.getElementById("cantidad10").value);
+   let preciototal10 = preciobase * cantidad;
+   document.getElementById("preciototal10").value = preciototal10;
+}
+
+function precio11(){
+   let preciobase = 30000;
+   let cantidad = parseInt(document.getElementById("cantidad11").value);
+   let preciototal11 = preciobase * cantidad;
+   document.getElementById("preciototal11").value = preciototal11;
+}
+
+function precio12(){
+   let preciobase = 35000;
+   let cantidad = parseInt(document.getElementById("cantidad12").value);
+   let preciototal12 = preciobase * cantidad;
+   document.getElementById("preciototal12").value = preciototal12;
+}
+
+function precio13(){
+   let preciobase = 35000;
+   let cantidad = parseInt(document.getElementById("cantidad13").value);
+   let preciototal13 = preciobase * cantidad;
+   document.getElementById("preciototal13").value = preciototal13;
+}
+
+function precio14(){
+   let preciobase = 39000;
+   let cantidad = parseInt(document.getElementById("cantidad14").value);
+   let preciototal14 = preciobase * cantidad;
+   document.getElementById("preciototal14").value = preciototal14;
+}
+
+function precio15(){
+   let preciobase = 22000;
+   let cantidad = parseInt(document.getElementById("cantidad15").value);
+   let preciototal15 = preciobase * cantidad;
+   document.getElementById("preciototal15").value = preciototal15;
+}
+
+function precio16(){
+   let preciobase = 45000;
+   let cantidad = parseInt(document.getElementById("cantidad16").value);
+   let preciototal16 = preciobase * cantidad;
+   document.getElementById("preciototal16").value = preciototal16;
+}
+
+function precio17(){
+   let preciobase = 51000;
+   let cantidad = parseInt(document.getElementById("cantidad17").value);
+   let preciototal17 = preciobase * cantidad;
+   document.getElementById("preciototal17").value = preciototal17;
+}
+
+function precio18(){
+   let preciobase = 20;
+   let cantidad = parseInt(document.getElementById("cantidad18").value);
+   let preciototal18 = preciobase * cantidad;
+   document.getElementById("preciototal18").value = preciototal18;
+}
+
+function precio19(){
+   let preciobase = 20;
+   let cantidad = parseInt(document.getElementById("cantidad19").value);
+   let preciototal19 = preciobase * cantidad;
+   document.getElementById("preciototal19").value = preciototal19;
+}
+
+function precio20(){
+   let preciobase = 20;
+   let cantidad = parseInt(document.getElementById("cantidad20").value);
+   let preciototal20 = preciobase * cantidad;
+   document.getElementById("preciototal20").value = preciototal20;
+}
+
+function precio21(){
+   let preciobase = 20;
+   let cantidad = parseInt(document.getElementById("cantidad21").value);
+   let preciototal21 = preciobase * cantidad;
+   document.getElementById("preciototal21").value = preciototal21;
+}
+
+function precio22(){
+   let preciobase = 20;
+   let cantidad = parseInt(document.getElementById("cantidad22").value);
+   let preciototal22 = preciobase * cantidad;
+   document.getElementById("preciototal22").value = preciototal22;
+}
+
+function precio23(){
+   let preciobase = 20;
+   let cantidad = parseInt(document.getElementById("cantidad23").value);
+   let preciototal23 = preciobase * cantidad;
+   document.getElementById("preciototal23").value = preciototal23;
+}
+
+function precio24(){
+   let preciobase = 20;
+   let cantidad = parseInt(document.getElementById("cantidad24").value);
+   let preciototal24 = preciobase * cantidad;
+   document.getElementById("preciototal24").value = preciototal24;
+}
